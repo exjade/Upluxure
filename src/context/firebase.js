674578b7381dc77fcd22378f783dec4b00fd47eb
Adrian Zamora <adrianzamora10@outlook.com>
2021-11-10 23:23:59 -0700

    Folder Structure & Architecture, Firestore, Firebase Auth

---
+++ src/context/firebase.js
@@ -0,0 +1,21 @@
+import { createContext } from 'react'
+
+const FirebaseContext = createContext(null);
+
+export default FirebaseContext;
+
+
+
+
+
+
+
+/* Provider-------------Component 1 -------------- (Firebase init here)*/
+/* -------------Component 2 -------------- */
+/* -------------Component 3 -------------- */
+/* consumer-------------Component 4 -------------- (Firebase init here)*/
+/* -------------Component 5 -------------- */
+/* -------------Component 6 -------------- */
+/* -------------Component 7 -------------- */
+/* -------------Component 8 -------------- */
+/* consumer-------------Component 9 -------------- (Firebase init here)*/
