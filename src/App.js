dce69c72c24022b395ed19643c0793ef5cd04a6d
Adrian Zamora <adrianzamora10@outlook.com>
2021-11-11 16:25:46 -0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
+import React, { lazy,Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Login from "./pages/Login";
+
+const Login = lazy(() => import("./pages/login"));
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola soy un H1</h1>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
 export default App;
 
 // 1) Client side rendered app: react (cra)
- // 2) -> database witch is Firebase
-   // 3) -> react-loading-skeleton
-    // 4) tailwind
+// 2) -> database witch is Firebase
+// 3) -> react-loading-skeleton
+// 4) tailwind
 
-
-// folder structure 
- // src
-  // -> components, 
-  // -> constants, 
-  // -> context,
-  // -> helpers,
-  // -> hooks,
-  // -> pages,
-  // -> lib (firebase is going to live in here),
-  // -> services (firebase functions in here),
-  // -> tailwind (tailwind's folder (app/tailwind))
+// folder structure
+// src
+// -> components,
+// -> constants,
+// -> context,
+// -> helpers,
+// -> hooks,
+// -> pages,
+// -> lib (firebase is going to live in here),
+// -> services (firebase functions in here),
+// -> tailwind (tailwind's folder (app/tailwind))
