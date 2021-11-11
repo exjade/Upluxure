190c8e196046cf39e083477928a75faefb747628
Adrian Zamora <adrianzamora10@outlook.com>
2021-11-10 23:42:29 -0700

    firebase connected

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,16 +2,25 @@ import Firebase from 'firebase/compat/app'
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+// import seed file
+import { seedDatabase } from '../seed';
+
 const config = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: "AIzaSyB9e3f7-KfrsZQRZVZB3KDz8Hb6DLhxIr4",
+  authDomain: "upluxure.firebaseapp.com",
+  projectId: "upluxure",
+  storageBucket: "upluxure.appspot.com",
+  messagingSenderId: "608331965920",
+  appId: "1:608331965920:web:c08403a7f130be1cef7785",
+  measurementId: "G-T0JKQPVQCB"
 };
 
 const firebase  = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
+
+// console.log("firebase", firebase)
+
+// call the seed file
+seedDatabase(firebase);
 
 export { firebase, FieldValue };
