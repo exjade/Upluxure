664b671c32310aa7ae50f9076bb93f219ece6dc8
Adrian Zamora <adrianzamora10@outlook.com>
2021-11-11 18:15:06 -0700

    Login page and Routes done

---
+++ src/constants/routes.js
@@ -0,0 +1,16 @@
+export const DASHBORAD = '/'
+export const LOGIN = '/login'
+export const SIGN_UP = '/signup'
+export const PROFILE = '/p/:username'
+export const NOT_FOUND = '/not-found'
+
+
+
+
+/* 
+/p/karl (:username)
+ -> get me :username = karl
+
+/p/steve (:username)
+ -> get me :username = steve
+*/
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState ,useContext, useEffect } from 'react'
+import { useHistory} from 'react-router-dom'
+import FirebaseContext from '../context/firebase'
 
 const Login = () => {
+
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [ emailAddress, setEmailAddress] = useState('');
+    const [ password, setPassword] = useState('');
+
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleLogin = (event) => {}
+
+    useEffect( () => {
+        document.title = 'Login - Upluxure';
+    }, [])
+    
     return (
-        <div>
-            I am the login page
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+            <p>I have no idea</p>
         </div>
     )
 }
