68499886f7e2f6230f3a95593fff26c438ec267c
Adrian Zamora <adrianzamora10@outlook.com>
2021-12-11 16:57:29 -0700

    loader in profile

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -1,56 +1,97 @@
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Skeleton from 'react-loading-skeleton'
 import styles from '../../styles/modules/profile/Photos.module.css'
+import ContentLoader from 'react-content-loader'
 
 const Photos = ({ photos }) => {
 
-    return (
-        <div className={`${styles.background} h-16 border-t border-gray-primary mt-12 pt-4`} >
-            <div className={`${styles.post} grid grid-cols-2 justify-around`}>
-                <h2 className={styles.title} >Photos</h2>
-                <p className={styles.amount} >{photos.length}</p>
-            </div>
-            <div className='grid grid-cols-2 gap-8 justify-between mt- p-3'>
-                {
-                    photos.slice(1, 2).map((photo, index) => (
-                        <div key={index} className=''>
-                            <img
-                                src={photo.imageSrc}
-                                alt={photo.caption}
-                                className={`${styles.mainimage}  object-cover p-2`}
-                            />
-                        </div>
-                    ))
-                }
-                {
-                    photos.slice(0, 1).map((photo, i) => (
-                        <div key={i} className={`${styles.imagesecondarycontainer}`} >
-                            <img
-                                src={photo.imageSrc}
-                                alt={photo.caption}
-                                className={`${styles.secondoneimage} object-cover p-2`}
-                            />
-                        </div>
-                    ))
-                }
-            </div>
-            <div className={` grid grid-cols-3 gap-5 mt-10 mb-2`} >
-                {!photos ? (
-                    <>
-                        <Skeleton count={12} height={320} width={400} />
-                    </>
-                ) : photos.length > 0 ?
-                    (
-                        photos.slice(2, 100).map((photo) => (
-                            <>
-                                <div key={photo.docId} className='relative group'>
-                                    <img
-                                        src={photo.imageSrc}
-                                        alt={photo.caption}
-                                        className={`${styles.image} w-full h-full object-cover p-3`}
-                                    />
 
-                                    <div className="absolute p-3 bottom-0 left-0 z-10 w-full justify-evenly items-center h-full bg-white-faded group-hover:flex">
+    /* LOADER Skeleton*/
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+
+        if (!photos) {
+            setIsLoading(true);
+        } else {
+            setTimeout(() => {
+                setIsLoading(false)
+            }, 1000);
+        }
+    }, [])
+    const loader = () => {
+        return (
+            <ContentLoader
+                width={1110}
+                height={575}
+                viewBox="0 0 800 575"
+                backgroundColor="#181818"
+                foregroundColor="#212121"
+            >
+                <rect x="12" y="58" rx="2" ry="2" width="211" height="211" />
+                <rect x="240" y="57" rx="2" ry="2" width="211" height="211" />
+                <rect x="467" y="56" rx="2" ry="2" width="211" height="211" />
+                <rect x="12" y="283" rx="2" ry="2" width="211" height="211" />
+                <rect x="240" y="281" rx="2" ry="2" width="211" height="211" />
+                <rect x="468" y="279" rx="2" ry="2" width="211" height="211" />
+            </ContentLoader>
+        )
+    }
+    if (isLoading) {
+        return loader()
+    } else {
+
+        return (
+            <div className={`${styles.background} h-16 border-t border-gray-primary mt-12 pt-4`} >
+                <div className={`${styles.post} grid grid-cols-2 justify-around`}>
+                    <h2 className={styles.title} >Photos</h2>
+                    <p className={styles.amount} >{photos.length}</p>
+                </div>
+                <div className='grid grid-cols-2 gap-8 justify-between mt- p-3'>
+                    {
+                        photos.slice(1, 2).map((photo, index) => (
+                            <div key={index} className=''>
+                                <img
+                                    src={photo.imageSrc}
+                                    alt={photo.caption}
+                                    className={`${styles.mainimage}  object-cover p-2`}
+                                />
+                            </div>
+                        ))
+                    }
+                    {
+                        photos.slice(0, 1).map((photo, i) => (
+                            <div key={i} className={`${styles.imagesecondarycontainer}`} >
+                                <img
+                                    src={photo.imageSrc}
+                                    alt={photo.caption}
+                                    className={`${styles.secondoneimage} object-cover p-2`}
+                                />
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className={` grid grid-cols-3 gap-5 mt-10 mb-2`} >
+                    {photos < 1 ? (
+                        <>
+                            {
+
+                                <Skeleton count={12} height={400} width={320} />
+
+                            }
+                        </>
+                    ) : photos.length > 0 ?
+                        (
+                            photos.slice(2, 100).map((photo) => (
+                                <>
+                                    <div key={photo.docId} className='relative group'>
+                                        <img
+                                            src={photo.imageSrc}
+                                            alt={photo.caption}
+                                            className={`${styles.image} w-full h-full object-cover p-3`}
+                                        />
+
+                                        {/* <div className="absolute p-3 bottom-0 left-0 z-10 w-full justify-evenly items-center h-full bg-white-faded group-hover:flex">
                                         <p className='flex items-center text-white-ctitle font-bold'>
 
                                             <svg
@@ -84,15 +125,16 @@ const Photos = ({ photos }) => {
                                             {photo.comments.length}
                                         </p>
 
+                                    </div> */}
                                     </div>
-                                </div>
-                            </>
-                        ))
-                    )
-                    : null}
+                                </>
+                            ))
+                        )
+                        : null}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Photos
