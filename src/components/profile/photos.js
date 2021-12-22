1d31591239c7e48f9179b51a17332cb04bf314f3
Adrian Zamora <adrianzamora10@outlook.com>
2021-12-21 22:22:41 -0700

    loaders in profile

--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -64,13 +64,20 @@ const Photos = ({ photos }) => {
                 }
             </div>
             <div className={` grid grid-cols-3 gap-1 mt-2 mb-2`} >
-                {photos < 1 ? (
+                {photos.length < 1 ? (
                     <>
                         {
-                            <Skeleton count={12} height={400} width={320} />
+                            <ContentLoader viewBox="0 0 820 450" height={450} width={1000}>
+                                <rect x="10" y="10" rx="5" ry="5" width="260" height="140" />
+                                <rect x="280" y="10" rx="5" ry="5" width="260" height="280" />
+                                <rect x="550" y="10" rx="5" ry="5" width="260" height="140" />
+                                <rect x="10" y="160" rx="5" ry="5" width="260" height="280" />
+                                <rect x="280" y="300" rx="5" ry="5" width="260" height="140" />
+                                <rect x="550" y="160" rx="5" ry="5" width="260" height="280" />
+                            </ContentLoader>
                         }
                     </>
-                ) : photos.length > 0 ?
+                ) : photos.length > 1 ?
                     (
                         photos.slice(2, 100).map((photo) => (
                             <>
