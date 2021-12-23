ee0354b044a71ab7491d0b0eddfc76179cf2830a
Adrian Zamora <adrianzamora10@outlook.com>
2021-12-23 12:20:05 -0700

    zoom to pinch module implemented in post img

--- src/components/post/image.js
+++ src/components/post/image.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import '../../styles/css/post/image.css'
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const Image = ({ src, caption }) => {
     return (
-        <div className="post__index_image">
-            {/* en img debe ir src => src={src} */}
-            <img 
-                src={src} 
-                alt={caption}
-                width="680"
-                height="680" 
-                className="post_index_img" /> 
-        </div>
+        <TransformWrapper>
+            <TransformComponent>
+                <div className="post__index_image">
+                    {/* en img debe ir src => src={src} */}
+                    <img
+                        src={src}
+                        alt={caption}
+                        width="680"
+                        height="680"
+                        className="post_index_img" />
+                </div>
+            </TransformComponent>
+        </TransformWrapper>
     )
 }
 
