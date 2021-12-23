3e1a9dcbdb826b30079516c69463784ec887fee1
Adrian Zamora <adrianzamora10@outlook.com>
2021-12-23 12:39:17 -0700

    zoom to pinch module disabled bug

--- src/components/post/image.js
+++ src/components/post/image.js
@@ -4,20 +4,29 @@ import '../../styles/css/post/image.css'
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const Image = ({ src, caption }) => {
+
+    function zoomOut(step, animationTime, animationName) {
+        step = 0.5
+        animationTime = 300
+        animationName = 'easeOut'
+    }
+
     return (
-        <TransformWrapper>
-            <TransformComponent>
-                <div className="post__index_image">
-                    {/* en img debe ir src => src={src} */}
-                    <img
-                        src={src}
-                        alt={caption}
-                        width="680"
-                        height="680"
-                        className="post_index_img" />
-                </div>
-            </TransformComponent>
-        </TransformWrapper>
+        <>
+
+            <div className="post__index_image">
+                {/* en img debe ir src => src={src} */}
+                <img
+                    src={src}
+                    alt={caption}
+                    width="680"
+                    height="680"
+                    className="post_index_img"
+                    onClick={() => zoomOut()}
+                />
+            </div>
+
+        </>
     )
 }
 
