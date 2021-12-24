3f738e9a65d7b19be35f4d660c69b0690cbaa3ee
Adrian Zamora <adrianzamora10@outlook.com>
2021-12-23 22:55:47 -0700

    refactorizacion de codigo

--- src/components/messages/index.js
+++ src/components/messages/index.js
@@ -1,14 +1,40 @@
+import { useEffect, useContext, useState } from 'react'
 import Header from './header'
 import Message from './message'
 import SearchBarDown from '../searchbardown'
+import UserContext from '../../context/user'
+/* Firebase */
+import { firebase } from '../../lib/firebase'
+import { getFirestore, collection, query, where, onSnapshot } from 'firebase/firestore'
+const firestore = getFirestore(firebase)
 
 const Messages = () => {
+    const { user, user: { uid } } = useContext(UserContext)
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const usersRef = collection(firestore, 'users')
+        // create a query object
+        const q = query(usersRef, where('userId', 'not-in', [uid]))
+        // execute query
+        const unsub = onSnapshot(q, (querySnapshot) => {
+            let users = []
+            querySnapshot.forEach(doc => {
+                users.push(doc.data())
+            })
+            setUsers(users)
+        })
+        return () => unsub()
+    }, [])
+    // console.log(users)
+    const premiumUsers = users.filter(user => user.rol !==  'free')
+    console.log(premiumUsers)
 
     return (
         <>
-            <Header />
+            <Header premiumUsers={premiumUsers}/>
             <Message />
-            <SearchBarDown />
+            <SearchBarDown userSearch={premiumUsers}/>
         </>
     )
 
--- src/components/messages/searchbar.js
+++ src/components/messages/searchbar.js
@@ -1,11 +1,16 @@
-import React from 'react'
 import '../../styles/css/messenger/searchbar.css'
 
-const SearchBar = () => {
+const SearchBar = ({ userSearch }) => {
+
+
     return (
         <div className="notfound_searchbox_inbox">
             <div className="notfound_searchbox_card_inbox ">
-                <input type="text" placeholder="Type something..." className="searchbar_input_inbox text-white-primary" />
+                <input
+                    type="text"
+                    placeholder="Type something..."
+                    className="searchbar_input_inbox text-white-primary"
+                />
             </div>
         </div>
     )
